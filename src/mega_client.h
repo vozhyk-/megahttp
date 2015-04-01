b69357bfd48b4e3635b52b8410ce1492d76aade9
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-04-01 22:10:42 +0200

    mega_client.h: Fix typo

--- src/mega_client.h
+++ src/mega_client.h
@@ -7,7 +7,7 @@ extern mega::MegaApi *mega_api;
 
 void init_mega();
 
-mega::MegaNode *get_mega_public_node(std::string url)
+mega::MegaNode *get_mega_public_node(std::string url);
 std::string download_public_file_to_tmp(std::string url);
 
 #endif // MEGA_CLIENT_H
