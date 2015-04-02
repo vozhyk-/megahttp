6003826d5b9675cb2094f06287b34bb21e44f092
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-04-02 21:13:58 +0200

    Revert "(DOSN'T COMPILE) chunked_test_resource: Start rewriting part of libhttpserver"
    This reverts commit e80aed2f42d55e438489dc079d0d36ede99931a7.

--- src/chunked_test_resource.h
+++ src/chunked_test_resource.h
@@ -5,23 +5,8 @@
 
 using namespace httpserver;
 
-class my_http_response_builder : public http_response_builder
-{
-    my_cycle_calback_ptr _my_cycle_callback;
-
-public:
-    http_response_builder &my_deferred_response(my_cycle_callback_ptr cb)
-    {
-        _my_cycle_callback = cb;
-        _get_raw_response = &http_response::get_raw_response_deferred;
-        _decorate_response = &http_response::decorate_response_deferred;
-        return *this;
-    }
-};
-
 class chunked_test_resource : public http_resource<chunked_test_resource>
 {
-    
 public:
     void render_GET(const http_request &, http_response **);
 };
