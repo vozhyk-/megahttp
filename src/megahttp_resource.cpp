bd704573181e8f2b84ec8db280b951d709808817
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-04-10 22:19:28 +0200

    Make response_callback a functor - fixes #8

--- src/megahttp_resource.cpp
+++ src/megahttp_resource.cpp
@@ -32,33 +32,43 @@ public:
 };
 */
 
-thread_local shared_ptr<file_cache_item> cached;
-thread_local size_t file_offset;
-
-
-ssize_t response_callback(char *out_buf, size_t max_size)
+class response_callback : public data_callback
 {
-    char *data;
-    ssize_t to_copy = cached->get_chunk(file_offset, max_size, data);
+    shared_ptr<file_cache_item> cached;
+    size_t file_offset;
 
-    cout << "HTTP data requested: file_offset " << file_offset
-         << ", max_size " << max_size
-         << ", to_copy " << to_copy
-         << ", data " << (void *)data
-         << ", filename ``" << cached->node->getName() << "''"
-         << endl;
-
-    if (to_copy > 0) // we got data
+public:
+    response_callback(shared_ptr<MegaNode> node)
+        : cached{file_cache[node]}, file_offset{0}
     {
-        memcpy(out_buf, data, to_copy);
-        file_offset += to_copy;
-    }
-    else if (to_copy == 0)
-    {
-        this_thread::sleep_for(http_response_sleep);
     }
 
-    return to_copy;
+    ssize_t operator()(char *out_buf, size_t max_size)
+    {
+        char *data;
+        ssize_t to_copy = cached->get_chunk(file_offset, max_size, data);
+
+        cout << "HTTP data requested: file_offset " << file_offset
+             << ", max_size " << max_size
+             << ", to_copy " << to_copy
+             << ", data " << (void *)data
+             << ", filename ``" << cached->node->getName() << "''"
+             << endl;
+
+        if (to_copy > 0) // we got data
+        {
+            memcpy(out_buf, data, to_copy);
+            file_offset += to_copy;
+        }
+        else if (to_copy == 0)
+        {
+            this_thread::sleep_for(http_response_sleep);
+        }
+
+        return to_copy;
+    };
+
+    ~response_callback() {}
 };
 
 void megahttp_resource::render_GET(const http_request &req, http_response **res)
@@ -82,14 +92,10 @@ void megahttp_resource::render_GET(const http_request &req, http_response **res)
 
     cout << "file size: " << node->getSize() << endl;
 
-    /* start node download */
-    /* must set these for callback to work */
-    file_offset = 0;
-    cached = file_cache[node];
-
     // TODO look at HTTP request range !
+    auto *cb = new response_callback(node);
 
     // associate http callback with http response
     *res = new http_response(http_response_builder("")
-                             .deferred_response(response_callback));
+                             .deferred_response(cb));
 }
