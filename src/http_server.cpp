751716725158fa8ed10b9a0a97ae991913e312ee
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-04-10 22:08:01 +0200

    Remove chunked_test_resource

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -3,7 +3,6 @@
 #include <httpserver.hpp>
 
 #include "megahttp_resource.h"
-#include "chunked_test_resource.h"
 
 using namespace std;
 using namespace httpserver;
@@ -14,9 +13,7 @@ void start_http_server()
         create_webserver(server_port).max_threads(server_max_threads);
 
     megahttp_resource resource;
-    chunked_test_resource ct_resource;
     server.register_resource("/", &resource, true);
-    server.register_resource("/chunked_test", &ct_resource, true);
 
     server.start(true);
 }
